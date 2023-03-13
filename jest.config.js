f9a858952222c77703db3e3a8f7bb181accc23a0
Noel Llevares <1972844+dashmug@users.noreply.github.com>
2023-03-14 00:39:38 +1100

    refactor: Colocate tests inside src

--- jest.config.js
+++ jest.config.js
@@ -2,8 +2,7 @@ module.exports = {
   testEnvironment: "node",
   collectCoverageFrom: ["src/**/*.ts"],
   coverageReporters: ["lcov", "text"],
-  testMatch: ["**/*.test.ts"],
-  setupFilesAfterEnv: ["<rootDir>/test/setup.js"],
+  setupFilesAfterEnv: ["<rootDir>/jest.setup.js"],
   transform: {
     "^.+\\.tsx?$": [
       "ts-jest",
@@ -16,6 +15,5 @@ module.exports = {
   moduleNameMapper: {
     "^bin/(.*)$": "<rootDir>/bin/$1",
     "^src/(.*)$": "<rootDir>/src/$1",
-    "^test/(.*)$": "<rootDir>/test/$1",
   },
 };
