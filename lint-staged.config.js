35575823280284d04c59633e57b0604fb4949079
Noel Llevares <1972844+dashmug@users.noreply.github.com>
2023-07-22 07:29:04 +1000

    ci: Switch to pnpm

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   "*.js": ["prettier --write", "eslint --format=pretty --quiet --fix --cache"],
-  "*.ts": () => ["yarn format", "yarn typecheck", "yarn test --only-changed"],
+  "*.ts": () => ["pnpm format", "pnpm typecheck", "pnpm test --only-changed"],
   "*.{json,yml,md}": "prettier --write --ignore-unknown",
   "*.sh": "shellcheck",
 };
